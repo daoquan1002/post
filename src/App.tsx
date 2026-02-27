6647bcbd3dfdd72960055a7bf8ac9ec42f281fa0
Quan. <daoquan1002@gmail.com>
2026-02-27 22:43:49 +0700

    Cập nhật

--- src/App.tsx
+++ src/App.tsx
@@ -38,7 +38,7 @@ const POSTS = [
     content: "kamome sano / happy ending",
     time: "27/02/2026",
     media: [
-      { type: "audio", url: "https://drive.google.com/uc?export=download&id=1_9VN7mdWrDNUVkrwd4Z3DkziaJXDRUDK" },
+      { type: "audio", url: "https://drive.google.com/uc?id=1_9VN7mdWrDNUVkrwd4Z3DkziaJXDRUDK" },
     ]
   },
   {
